e4770027e41345b5740a758355b268f66931bf23
Joshua Westerheide <dev@jdoubleu.de>
2019-02-06 00:40:50 +0100

    Implement Hyphenator class/helper

---
+++ src/lib/Hyphenator.js
@@ -0,0 +1,61 @@
+import Hypher from 'hypher'
+import german from 'hyphenation.de'
+
+const MonokaiColors = ['#272822', '#F92672', '#66D9EF', '#A6E22E', 'FD971F']
+
+function tokenizeText(text) {
+  const reTextTokens = /(\s*[.,;:!?\-\s\n\t]\s*)/
+  const rePunctuation = /[^.,;:?!\-\s\n\t]/
+
+  return text.split(reTextTokens)
+    .map(value => {
+      return {
+        type: value.match(rePunctuation) ? 'word' : 'punc',
+        value
+      }
+    })
+}
+
+class Hyphenator {
+  constructor(colors) {
+    this.h = new Hypher(german)
+    this.colors = colors || MonokaiColors
+  }
+
+  hyphenText(text) {
+    return tokenizeText(text)
+      .map(this._hyphenToken.bind(this))
+      .map(this._colorizeToken.bind(this))
+      .map(token => token.value)
+      .join('')
+      .replace(/\n/g, '<br>')
+  }
+
+  _hyphenToken(token) {
+    if (token.type !== 'word') {
+      return token
+    }
+
+    token.value = this.h.hyphenate(token.value)
+    return token
+  }
+
+  _getColor(index) {
+    return this.colors[ index % this.colors.length ]
+  }
+
+  _colorizeToken(token) {
+    if (token.type !== 'word') {
+      return token
+    }
+
+    token.value = token.value.reduce(
+      (acc, hyphen, index) => `${acc}<span style="color: ${this._getColor(index)}">${hyphen}</span>`,
+      ''
+    )
+
+    return token
+  }
+}
+
+export default Hyphenator
