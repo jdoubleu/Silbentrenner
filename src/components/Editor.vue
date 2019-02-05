65a1460b87ba8a392a6a4952d060ed29762cc7db
Joshua Westerheide <dev@jdoubleu.de>
2019-02-05 18:56:35 +0100

    Add Editor component

---
+++ src/components/Editor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="editor">
+    <textarea v-model="value" placeholder="Eingabe" autofocus required></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  watch: {
+    value (value) {
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style>
+#editor textarea {
+  border: 0;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  padding: 1.5rem;
+  width: 100%;
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
