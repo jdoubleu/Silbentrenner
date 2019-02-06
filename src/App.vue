721325055a2c8ebd141c6cce143ffb23fa9d0db4
Joshua Westerheide <dev@jdoubleu.de>
2019-02-06 11:40:56 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <Editor class="side" v-model="text"/>
-    <div id="output" class="side" v-html="output" @click="handleOutputClick"></div>
+    <header>
+      <h1>Silbentrenner</h1>
+      <span>language: german</span>
+      <a href="https://github.com/jdoubleu/Silbentrenner">source code</a>
+    </header>
+    <main>
+      <Editor v-model="text"/>
+      <div v-html="output" @click="handleOutputClick"></div>
+    </main>
   </div>
 </template>
 
@@ -54,14 +61,38 @@ html,
 body {
   margin: 0;
   padding: 0;
-  height: 100%;
   overflow: hidden;
 
   font-size: 18px;
   font-family: 'Monaco', courier, monospace;
 }
 
+html,
+body,
 #app {
+  height: 100%;
+}
+
+header,
+main {
+  position: relative;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75rem 1.5rem;
+  box-shadow: 0 0 5px #696969;
+  z-index: 10;
+}
+
+header h1 {
+  font-size: 1rem;
+  font-weight: normal;
+  margin: 0;
+}
+
+main {
   display: flex;
   align-items: stretch;
   justify-items: stretch;
@@ -69,12 +100,12 @@ body {
   hyphens: auto;
 }
 
-#app .side {
+main > * {
   flex: 1;
   padding: 1.5rem;
 }
 
-#app .side:first-child {
+main > *:first-child {
   border-right: 1px solid grey;
 }
 </style>
