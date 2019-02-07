40ceeef390b30a5da5f957e574332b3c9874e375
Joshua Westerheide <dev@jdoubleu.de>
2019-02-07 13:30:40 +0100

    Include BSD 3-Clause and LGPG licenses of hyper in vendor chunk
    Closes #2

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
+const BannerPlugin = require('webpack').BannerPlugin
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Silbentrenner/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Silbentrenner/' : '/',
+  chainWebpack: config => {
+    config.plugin('hypher-license')
+      .use(BannerPlugin, [{
+        test: /chunk-vendors/,
+        banner: `Hypher (https://github.com/bramstein/hypher)
+Copyright (c) 2011, Bram Stein
+Licensed under three clause BSD (https://github.com/bramstein/hypher/blob/master/BSD.txt)
+
+hyphenation-patters (german version, 'de') (https://github.com/bramstein/hyphenation-patterns)
+Licensed under LGPL (https://github.com/bramstein/hyphenation-patterns#license)`
+      }])
+  }
 }
